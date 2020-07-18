c8537a435f52ee9583ed4638a4470463be0c31dd
Gad Wissberg <gadw17@gmail.com>
2020-07-18 13:14:58 +0300

    Initial commit.

--- src/main/java/TaskWrapper.java
+++ src/main/java/TaskWrapper.java
@@ -1,32 +1,77 @@
 import lombok.Getter;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 
 @Getter
-public class TaskWrapper<T> extends FutureTask<T> implements Comparable<TaskWrapper<T>> {
-    private final TaskType taskType;
+public class TaskWrapper<T> implements RunnableFuture<T>, Comparable<TaskWrapper<T>> {
+	private final TaskType taskType;
+	private final FutureTask<T> task;
 
-    public TaskWrapper(final TaskType taskType, final Runnable runnable, final T result) {
-        super(runnable, result);
-        this.taskType = taskType;
-    }
 
-    public TaskWrapper(final TaskType taskType, final Callable<T> callable) {
-        super(callable);
-        this.taskType = taskType;
-    }
+	public TaskWrapper(final TaskType taskType, final Runnable runnable) {
+		this.taskType = taskType;
+		this.task = new FutureTask<>(runnable, null);
+	}
 
-    @Override
-    public int compareTo(final TaskWrapper<T> tTaskWrapper) {
-        return Integer.compare(taskType.getPriority(), tTaskWrapper.getTaskType().getPriority());
-    }
+	public TaskWrapper(final TaskType taskType, final Callable<T> callable) {
+		this.taskType = taskType;
+		this.task = new FutureTask<>(callable);
+	}
 
-    @Override
-    public String toString() {
-        return "TaskWrapper{" +
-                "taskType=" + taskType +
-                '}';
-    }
+	@Override
+	public int compareTo(final TaskWrapper<T> tTaskWrapper) {
+		return Integer.compare(taskType.getPriority(), tTaskWrapper.getTaskType().getPriority());
+	}
+
+	@Override
+	public String toString() {
+		return "TaskWrapper{" +
+				"taskType=" + taskType +
+				", task=" + task +
+				'}';
+	}
+
+	@Override
+	public void run() {
+		task.run();
+	}
+
+	@Override
+	public boolean cancel(final boolean mayInterruptIfRunning) {
+		return task.cancel(mayInterruptIfRunning);
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return task.isCancelled();
+	}
+
+	@Override
+	public boolean isDone() {
+		return task.isDone();
+	}
+
+	@Override
+	public T get() {
+		T result = null;
+		try {
+			result = task.get();
+		} catch (final InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public T get(final long l, final TimeUnit timeUnit) {
+		T result = null;
+		try {
+			result =  task.get(l,timeUnit);
+		} catch (final InterruptedException | ExecutionException | TimeoutException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 }
